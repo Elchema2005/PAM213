8c1367c1635ecc0332ed4540668360394d070a78
Elchema2005 <124051357@upq.edu.mx>
2025-09-10 16:08:59 -0600

    No.11estadisticas de juego

--- JS para principiantes/app.js
+++ JS para principiantes/app.js
@@ -1,11 +1,9 @@
-let contraseña = "pepe"
-let input = "pep5e"
-let resultado = input == contraseña;
-if (resultado == true) {
-    console.log("login correcto");
-
-if (resultado == false)
-    console.log("login incorrecto");
-} else {
-    console.log("contraseña incorrecta");
+let score = 70;
+if (score > 30){
+    console.log("Practica mas");
+} else if(score > 15){
+    console.log("estas mejorando");
+}
+else{
+    console.log("practicamas con este tutorial");
 }
