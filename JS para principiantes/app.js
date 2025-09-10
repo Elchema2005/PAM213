f9e0cd4829cdf222746d91eb90ca503394538c87
Elchema2005 <124051357@upq.edu.mx>
2025-09-10 15:20:31 -0600

    No.6 tipos de variable

--- JS para principiantes/app.js
+++ JS para principiantes/app.js
@@ -1,8 +1,26 @@
+//estoy creando variables
 var nombre = "john";
 let apellido ="carter"
 apellido ="pepe";
 console.log(apellido)
 
+//Reasignando la variable nameuser
+
 const PI =3.1416;
 //PI = 100
+
 console.log(PI)
+
+//1username = "fazt";
+//let nicknme ="fazt";
+
+//camelcase
+//let nombreDePersona = "gordon";
+//nombredepersona;
+//user_name
+
+
+/*gifuhifvkvhbijrfjkrfrfrfgfrg
+rgrgrgrgrgrrtrt
+rtrtrtrtrtrtrtjlkjhlhyhyhyhy
+*/
