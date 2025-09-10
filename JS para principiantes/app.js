5d50c44429c80925c522edf95cd60b6c86692ef2
Elchema2005 <124051357@upq.edu.mx>
2025-09-10 14:52:15 -0600

    No.4objetos

--- JS para principiantes/app.js
+++ JS para principiantes/app.js
@@ -1,4 +1,21 @@
-//arreglo
-["jose","rayan","martyn"]
-[1,2,3]
-[true,false,true,false]
+//objeto
+"rayan"
+70.4
+14
+true
+{
+    "sername": "rayan",
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true,
+    "friends": [" "," "," ",]
+
+}
+{
+    "sername": "joe",
+    "score": 0.4,
+    "friends": [" "," "," ",]
+
+}
+console.log([1,2,3])
+console.log({"username":"rayan","score":70.4});
