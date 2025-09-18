81f8d2573547fba652e33f610d766174286f9149
Elchema2005 <124051357@upq.edu.mx>
2025-09-18 10:15:19 -0600

    Saludo personalizado

---
+++ Practica1/SaludoPersonalizado.js
@@ -0,0 +1,7 @@
+let SaludoPersonalizado = "Armando";
+let edad = 37;
+
+nombre = "Isay";
+const saludo = (nombre, edad) => "Hola,me llamo " + nombre + " y Tengo " + edad + " años.";
+
+console.log(saludo(nombre,edad));
