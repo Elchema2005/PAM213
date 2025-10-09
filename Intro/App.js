f431276e5d96cb2f7160882424fcff8c517e5e33
Elchema2005 <124051357@upq.edu.mx>
2025-10-08 20:17:10 -0600

    Botones

--- Intro/App.js
+++ Intro/App.js
@@ -1,15 +1,28 @@
+//zona 1 import: Zona de importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import React,{useState} from 'react';
 
+//zona 2 main: Zona de componentes
 export default function App() {
+
+  const [contador,setContador]=useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo</Text>
-      <StatusBar style="auto" />
+      <Text>Contador: {contador} </Text>      {/*shift + alt + A */}
+      <Button title="Agregar" onPress={()=>setContador(contador + 1)}/>
+
+        <Button title="Quitar" onPress={()=>setContador(contador - 1)}/>
+          
+          <Button title="Reiniciar" onPress={()=>setContador(contador * 0)}/>
+
+    <StatusBar style="auto" />
     </View>
   );
 }
 
+//Zona 3: Zona de los estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
