f149c606de833cf55b6a9dc1cf642df9d9aad3ba
Elchema2005 <124051357@upq.edu.mx>
2025-10-09 19:26:51 -0600

    Prectica No.6

--- Intro/App.js
+++ Intro/App.js
@@ -1,33 +1,8 @@
-//zona 1 import: Zona de importaciones
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button } from 'react-native';
-import React,{useState} from 'react';
+import Contador from './Screen/Contador';
 
 //zona 2 main: Zona de componentes
 export default function App() {
-
-  const [contador,setContador]=useState(0);
-
   return (
-    <View style={styles.container}>
-      <Text>Contador: {contador} </Text>      {/*shift + alt + A */}
-      <Button title="Agregar" onPress={()=>setContador(contador + 1)}/>
-
-        <Button title="Quitar" onPress={()=>setContador(contador - 1)}/>
-          
-          <Button title="Reiniciar" onPress={()=>setContador(contador * 0)}/>
-
-    <StatusBar style="auto" />
-    </View>
+    <Contador></Contador>
   );
 }
-
-//Zona 3: Zona de los estilos
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
